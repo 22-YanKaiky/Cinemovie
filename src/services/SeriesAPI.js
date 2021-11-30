bf1d004b916b33913e8dc60096d0c0e53ce95875
Yan Kaiky <yankaikys@gmail.com>
2021-11-30 20:37:32 -0300

    feat(Services): SeriesAPI

--- src/services/SeriesAPI.js
+++ src/services/SeriesAPI.js
@@ -1,13 +1,5 @@
 import api_url from "./api_url";
-import axios from  'axios'
+
 const SERIESAPI = api_url + "/series"
 
-export function videoPost(props){
-    axios.post(`${SERIESAPI}`, props)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });         
-}
+export default SERIESAPI;
