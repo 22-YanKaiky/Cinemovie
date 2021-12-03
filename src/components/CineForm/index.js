2bb00c1753490b22a820a85e67987c1cb8cebf40
Yan Kaiky <yankaikys@gmail.com>
2021-12-02 23:52:23 -0300

    feat(Series): useEffect

--- src/components/CineForm/index.js
+++ src/components/CineForm/index.js
@@ -1,50 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import AnimesAPI from "../../services/AnimesAPI";
 // import MoviesAPI from "../../services/MoviesAPI";
 import SeriesAPI from "../../services/SeriesAPI";
 import styled from "styled-components";
 import axios from "axios";
-import { useEffect } from "react/cjs/react.development";
 
 export default function CineForm() {
-    const listObj = {
-        type: "",
-        name: "",
-        src: "",
-        year: 0,
-        genre: "",
-        seasons: 0,
-        episodes: 0,
-        direction: "",
-        synopsis: "",
-        trailer: "",
-        link: ""
+    let videoObj = {
+        type: '',
+        name: '',
+        src: '',
+        year: '',
+        genre: '',
+        seasons: '',
+        episodes: '',
+        folder: '',
+        direction: '',
+        synopsis: '',
+        trailer: '',
+        link: ''
     }
 
-    const [video, setVideo] = useState(listObj)
+    const [video, setVideo] = useState(videoObj);
     
     useEffect(() => {
-        async function onSubmit() {
-            await axios.post(SeriesAPI, {
-                video
-            })
-
-            setVideo()
+        async function fetchData() {
+            try {
+                const response = await axios.post(SeriesAPI, video);
+                
+                console.log(response.data);
+                
+                setVideo([...video, response.data])
+            } catch (e) {
+                console.log(e.message)
+            }
         }
-
-        onSubmit()
-    }, [video])
+        fetchData();
+    }, [video]);
 
     function handleChange(event) {
-        const {name, value} = event.target;
-
-        setVideo({...video,
-            [name]: value
-        })
+        const { name, value } = event.target;
+        setVideo((video) => {
+            return {
+                ...video,
+                [name]: value,
+            };
+        });
 
         console.log(event.target)
     }
-    
+
+
     return (
         <form>
             <Select onChange={handleChange} value={video.type} name="type">
@@ -54,18 +60,19 @@ export default function CineForm() {
                 <option value="series">Series</option>
             </Select>
 
-            <Input onChange={handleChange} value={video.name} name="name" placeholder="Nome"/>
-            <Input onChange={handleChange} value={video.src} name="src" placeholder="Link da Imagem"/>
-            <Input onChange={handleChange} value={video.year} name="year" placeholder="Ano"/>
-            <Input onChange={handleChange} value={video.genre} name="genre" placeholder="Genero"/>
-            <Input onChange={handleChange} value={video.seasons} name="seasons" placeholder="Temporadas"/>
-            <Input onChange={handleChange} value={video.episodes} name="episodes" placeholder="Episodios"/>
-            <Input onChange={handleChange} value={video.direction} name="direction" placeholder="Direcao"/>
-            <Input onChange={handleChange} value={video.synopsis} name="synopsis" placeholder="Sinopse"/>
-            <Input onChange={handleChange} value={video.trailer} name="trailer" placeholder="Trailer"/>
-            <Input onChange={handleChange} value={video.link} name="link" placeholder="Link do Video"/>
+            <Input onChange={handleChange} value={video.name} name="name" placeholder="Nome" />
+            <Input onChange={handleChange} value={video.src} name="src" placeholder="Link da Imagem" />
+            <Input onChange={handleChange} value={video.year} name="year" placeholder="Ano" />
+            <Input onChange={handleChange} value={video.genre} name="genre" placeholder="Genero" />
+            <Input onChange={handleChange} value={video.seasons} name="seasons" placeholder="Temporadas" />
+            <Input onChange={handleChange} value={video.episodes} name="episodes" placeholder="Episodios" />
+            <Input onChange={handleChange} value={video.folder} name="folder" placeholder="Folder" />
+            <Input onChange={handleChange} value={video.direction} name="direction" placeholder="Direcao" />
+            <Input onChange={handleChange} value={video.synopsis} name="synopsis" placeholder="Sinopse" />
+            <Input onChange={handleChange} value={video.trailer} name="trailer" placeholder="Trailer" />
+            <Input onChange={handleChange} value={video.link} name="link" placeholder="Link do Video" />
 
-            <Button type="submit">Enviar</Button>
+            <Button onSubmit={setVideo} type="submit">Enviar</Button>
         </form>
     )
 }
