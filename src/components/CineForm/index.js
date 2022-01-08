d1161af84113d1a637db902b84865288caa27abf
22-YanKaiky <yankaikys@gmail.com>
2022-01-08 17:50:52 -0300

    feat(Cineform): useEffect

--- src/components/CineForm/index.js
+++ src/components/CineForm/index.js
@@ -22,55 +22,56 @@ export default function CineForm() {
     }
 
     const [video, setVideo] = useState(videoObj);
-        
+    // const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.post(SeriesAPI, video);
+                const result = await axios.post(SeriesAPI, video);
 
-                console.log(response.data);
+                console.log(result.data);
             } catch (e) {
                 console.log(e.message)
             }
         }
 
         fetchData();
-    }, [video]);
+    }, []);
 
-    function handleChange(event) {
+    const setInput = (event) => {
         const { name, value } = event.target;
-        setVideo((video) => {
-            return {
-                ...video,
-                [name]: value,
-            };
-        });
+        const newVideo = {
+            ...video,
+            [name]: value,
+        };
+
+        setVideo(newVideo)
 
         console.log(event.target)
     }
 
     return (
         <form>
-            <Select onChange={handleChange} value={video.type} name="type">
+            <Select onChange={setInput} value={video.type} name="type">
                 <option disabled>Selecione</option>
                 <option value="anime">Anime</option>
                 <option value="movies">Filmes</option>
                 <option value="series">Series</option>
             </Select>
 
-            <Input onChange={handleChange} value={video.name} name="name" placeholder="Nome" />
-            <Input onChange={handleChange} value={video.src} name="src" placeholder="Link da Imagem" />
-            <Input onChange={handleChange} value={video.year} name="year" placeholder="Ano" />
-            <Input onChange={handleChange} value={video.genre} name="genre" placeholder="Genero" />
-            <Input onChange={handleChange} value={video.seasons} name="seasons" placeholder="Temporadas" />
-            <Input onChange={handleChange} value={video.episodes} name="episodes" placeholder="Episodios" />
-            <Input onChange={handleChange} value={video.folder} name="folder" placeholder="Folder" />
-            <Input onChange={handleChange} value={video.direction} name="direction" placeholder="Direcao" />
-            <Input onChange={handleChange} value={video.synopsis} name="synopsis" placeholder="Sinopse" />
-            <Input onChange={handleChange} value={video.trailer} name="trailer" placeholder="Trailer" />
-            <Input onChange={handleChange} value={video.link} name="link" placeholder="Link do Video" />
+            <Input onChange={setInput} value={video.name} name="name" placeholder="Nome" />
+            <Input onChange={setInput} value={video.src} name="src" placeholder="Link da Imagem" />
+            <Input onChange={setInput} value={video.year} name="year" placeholder="Ano" />
+            <Input onChange={setInput} value={video.genre} name="genre" placeholder="Genero" />
+            <Input onChange={setInput} value={video.seasons} name="seasons" placeholder="Temporadas" />
+            <Input onChange={setInput} value={video.episodes} name="episodes" placeholder="Episodios" />
+            <Input onChange={setInput} value={video.folder} name="folder" placeholder="Folder" />
+            <Input onChange={setInput} value={video.direction} name="direction" placeholder="Direcao" />
+            <Input onChange={setInput} value={video.synopsis} name="synopsis" placeholder="Sinopse" />
+            <Input onChange={setInput} value={video.trailer} name="trailer" placeholder="Trailer" />
+            <Input onChange={setInput} value={video.link} name="link" placeholder="Link do Video" />
 
-            <Button onChange={setVideo} type="submit">Enviar</Button>
+            <Button type="submit">Enviar</Button>
         </form>
     )
 }
