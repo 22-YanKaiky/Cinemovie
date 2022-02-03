fd23fea0befa8e89f4942f7c320a3edbf22ab713
Yan Kaiky <yankaikys@gmail.com>
2022-02-02 22:49:36 -0300

    feat(CineForm): format link trailer

--- src/components/CineForm/index.js
+++ src/components/CineForm/index.js
@@ -64,12 +64,21 @@ export default function CineForm() {
 
     const [sending, setSending] = useState(false);
 
+    const trailerFormat = (val) => {
+        const link = val.split('/')[3]
+
+        return `https://www.youtube.com/embed/${link}`
+    }
+
     const sendRequest = async () => {
         if (sending) return;
         setSending(true);
         const videoReq = {
             ...video,
+            trailer: trailerFormat(video.trailer)
         };
+
+        console.log('videoReq', videoReq)
 
         await axios.post(api, videoReq);
 
@@ -89,7 +98,7 @@ export default function CineForm() {
                     <option selected>- Selecione -</option>
                     <option value="anime">Anime</option>
                     <option value="movies">Filmes</option>
-                    <option value="series">Series</option>
+                    <option value="series">Séries</option>
                 </Select>
             </Div>
 
